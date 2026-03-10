acca2a193732a270de4bc391e679b99549475558
Eric Lavault <39483232+lvlte@users.noreply.github.com>
2026-03-10 21:09:35 +0100

    Use `nothing` as intermediate default for `tol` param
    This allows to define distinct methods for when the default should
    be used vs when a custom `tol` is given.
    Also rename/fix `eps` for irrationals: in the previous commit the
    function would always return a Float64 and thus could return `0.0`
    with higher BigFloat precision (ie. `2.0^-e == 0.0` for e > 1074).

--- base/irrationals.jl
+++ base/irrationals.jl
@@ -62,8 +62,8 @@ Complex{T}(x::AbstractIrrational) where {T<:Real} = Complex{T}(T(x))
 
 function _irrational_to_rational_at_current_precision(::Type{T}, x::AbstractIrrational) where {T <: Integer}
     bx = BigFloat(x)
-    r = rationalize(T, bx, _eps(x)/2)
-    if abs(BigFloat(r) - bx) > _eps(x)
+    r = rationalize(T, bx, eps(x)/2)
+    if abs(BigFloat(r) - bx) > eps(x)
         r
     else
         nothing  # Error is too small, repeat with greater precision.
@@ -145,20 +145,23 @@ function _throw_rationalize_irrational_zero_tol_bigint()
 end
 
 # Irrational vs Rational
+function _rationalize_irrational(::Type{T}, x::AbstractIrrational, tol::Nothing) where {T<:Integer}
+    return rationalize(T, big(x), eps(x)/2)
+end
 function _rationalize_irrational(::Type{T}, x::AbstractIrrational, tol::Real) where {T<:Integer}
     if iszero(tol)
         T === BigInt && _throw_rationalize_irrational_zero_tol_bigint()
         return _irrational_to_rational(T, x)
     end
-    tol ≥ eps(Float64(x)) && return rationalize(T, Float64(x), tol/2)
-    tol ≥ _eps(x)/2 && return rationalize(T, big(x), tol)
+    tol ≥ eps(Float64, x) && return rationalize(T, Float64(x), tol/2)
+    tol ≥ eps(BigFloat, x)/2 && return rationalize(T, big(x), tol)
     # need more precision
     p = exponent(x) - exponent(tol) + 8
     setprecision(BigFloat, p) do
         return rationalize(T, big(x), tol)
     end
 end
-function rationalize(::Type{T}, x::AbstractIrrational; tol::Real=_eps(x)/2) where {T<:Integer}
+function rationalize(::Type{T}, x::AbstractIrrational; tol::Union{Nothing,Real}=nothing) where {T<:Integer}
     return _rationalize_irrational(T, x, tol)
 end
 function rationalize(::Type{T}, x::AbstractIrrational, tol::Real) where {T<:Integer}
@@ -316,5 +319,7 @@ end
 # inv
 inv(x::AbstractIrrational) = 1/x
 
-# equivalent to Float64(eps(big(x)))
-_eps(x::AbstractIrrational) = 2.0^(exponent(x) - precision(BigFloat) + 1)
+eps(x::AbstractIrrational) = eps(BigFloat, x)
+function eps(::Type{T}, x::AbstractIrrational) where {T<:AbstractFloat}
+    return T(2)^(exponent(x) - precision(T) + 1)
+end
